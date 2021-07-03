c506b80b6bb7ebfaf9d496d1317760880ab35962
醒言 <orcexzy@163.com>
2021-07-04 00:40:31 +0800

    andtest

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2021/4/19 0019
+
+@author: xing yan
+"""
---
+++ app/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2021/4/19 0019
+
+@author: xing yan
+"""
+
+p = ""
+
